e0d1b1df55ff37f9f7480176d349087075aa2ee5
kohei0802 <koheileung0208@gmail.com>
2025-07-16 23:25:14 +0800

    removed all famous errors. Trying to increase depth to 12, reducing russian to 0.8

--- Scene.cpp
+++ Scene.cpp
@@ -60,54 +60,9 @@ bool Scene::trace(
     return (*hitObject != nullptr);
 }
 
-// Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
-    // // TO DO Implement Path Tracing Algorithm here
-
-    // if (depth == 0)
-    // {
-    //     Intersection intersection = Scene::intersect(ray);
-
-    //     if (intersection.happened)
-    //     {
-    //         if (intersection.m->hasEmission())
-    //         {
-    //             return intersection.m->getEmission() ;
-    //         }
-    //         return castRay(ray, depth+1);
-
-    //     } 
-
-    //     return Vector3f(0);
-    // }
-
-    // else 
-    // {
-    //     Intersection intersection = Scene::intersect(ray);
-
-    //     // double check that this is not light source
-    //     Material* m = intersection.m;
-
-    //     // light source sampling
-    //     Intersection light_sample;
-    //     float light_pdf;
-    //     sampleLight(light_sample, light_pdf);
-    //     Vector3f old_ws = light_sample.coords - intersection.coords;
-
-    //     Vector3f ws = normalize(old_ws);
-    //     float cos_surface = std::max(0.0f, dotProduct(ws, intersection.normal));
-    //     float cos_light = std::max(0.0f, dotProduct(-ws, light_sample.normal));
-
-    //     Vector3f l_direct = light_sample.emit * 
-    //                m->eval(ws, -ray.direction, intersection.normal) * 
-    //                cos_surface * cos_light /
-    //                (dotProduct(old_ws, old_ws) * light_pdf);
-
-    //     return l_direct;  // Fix this too!  
-    // }
-
-    if (depth > 3)
+    if (depth > 12)
     {
         return Vector3f(0);
     }
@@ -133,26 +88,30 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
     Vector3f p_to_light = sample_light.coords - intersection.coords;
     Vector3f wi = normalize(p_to_light);
 
-    
-    Vector3f offset_coord = intersection.coords + intersection.normal * 0.0001f;
+    // ADD LIGHT VISIBILITY CHECK HERE
+    if (dotProduct(wi, intersection.normal) > 0) {  // Light is visible from this side of surface
+        Vector3f offset_coord = intersection.coords + intersection.normal * 0.001f;
 
-    Intersection block_check = Scene::intersect(Ray(offset_coord, wi, 0.0f));
+        Intersection block_check = Scene::intersect(Ray(offset_coord, wi, 0.0f));
 
-    Vector3f a = block_check.coords - intersection.coords;
-    Vector3f b = sample_light.coords - intersection.coords;
-    bool blocked = block_check.happened && dotProduct(a, a) < dotProduct(b, b) + 1e-5f;
-    if (!blocked)
-    {
-        l_dir = sample_light.emit * 
-                    intersection.m->eval(wi, -ray.direction, intersection.normal) * 
-                    dotProduct(wi, intersection.normal) * 
-                    dotProduct(-wi, sample_light.normal) / 
-                    (dotProduct(p_to_light, p_to_light) * pdf_light);
-    
+        Vector3f a = block_check.coords - intersection.coords;
+        Vector3f b = sample_light.coords - intersection.coords;
+        bool blocked = block_check.happened && dotProduct(a, a) < dotProduct(b, b) + 1e-4f;
+        
+        if (!blocked)
+        {
+            l_dir = sample_light.emit * 
+                        intersection.m->eval(wi, -ray.direction, intersection.normal) * 
+                        dotProduct(wi, intersection.normal) * 
+                        dotProduct(-wi, sample_light.normal) / 
+                        (dotProduct(p_to_light, p_to_light) * pdf_light);
+        }
     }
+    // If dotProduct(wi, intersection.normal) <= 0, l_dir remains Vector3f(0)
+
     // do indirect illumination sampling
     float random_float = dis(gen);
-    float russian_pdf = 0.85;
+    float russian_pdf = 0.8;
     
     if (random_float > russian_pdf)
     {
@@ -160,8 +119,7 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
     }
 
     Vector3f sample_dir = intersection.m->sample(wi, intersection.normal);
-    Vector3f offset_p = intersection.coords + intersection.normal * 0.0001f;
-
+    Vector3f offset_p = intersection.coords + intersection.normal * 0.001f;
 
     Vector3f l_indir = castRay(Ray(offset_p, sample_dir, 0.0f), depth+1) *
                        intersection.m->eval(sample_dir, -ray.direction, intersection.normal) *
